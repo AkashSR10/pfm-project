3286d18fab324ed49526a160aff310ae89e0126e
Ash2 <akashsr10@gmail.com>
2020-03-09 16:48:52 +0100

    Renewed Login and Signup UI

--- src/sample/controller/login/loginController.java
+++ src/sample/controller/login/loginController.java
@@ -3,6 +3,8 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,40 +26,37 @@ public class loginController {
     private URL location;
 
     @FXML
-    private Button loginButton;
-
-    @FXML
-    private PasswordField passwordText;
-
-    @FXML
-    private TextField emailText;
-
-    @FXML
-    private Button signupButton;
-
-    @FXML
     private Text error;
 
-    ViewSwitcher view = new ViewSwitcher();
+    @FXML
+    private JFXTextField emailField;
 
+    @FXML
+    private JFXPasswordField passwordField;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private JFXButton signupButton;
+
+    ViewSwitcher view = new ViewSwitcher();
 
     // Called when loginButton is pressed
     @FXML
    public void loadLogin(ActionEvent event) throws SQLException {
-        // Make new Objects
-        SQLite query = new SQLite();
         User user = new User();
 
         // Get text from inputs
-        user.email = emailText.getText().trim();
-        user.password = passwordText.getText().trim();
+        user.email = emailField.getText().trim();
+        user.password = passwordField.getText().trim();
 
         // Checks if combination is correct and a  member
-        if (query.memberLogin(user.email, user.password)) {
+        if (user.memberLogin(user.email, user.password)) {
             loginButton.getScene().getWindow().hide();
             view.memberMenu();
             // Checks if combination is correct and an admin
-        } else if (query.adminLogin(emailText.getText().trim(), passwordText.getText().trim())) {
+        } else if (user.adminLogin(emailField.getText().trim(), passwordField.getText().trim())) {
             loginButton.getScene().getWindow().hide();
             view.adminMenu();
             // Returns error if the user is not a member or admin
@@ -69,7 +68,7 @@ public class loginController {
     // Called when signupButton is pressed
     @FXML
     void loadSignup(ActionEvent event) {
-            signupButton.getScene().getWindow().hide();
+        signupButton.getScene().getWindow().hide();
             view.signup();
 
     }
--- src/sample/model/User.java
+++ src/sample/model/User.java
@@ -81,5 +81,90 @@ public class User {
         this.admin = admin;
     }
 
+    // FUNCTIONS SQL
+    ResultSet rs = null;                                                     // Declare resultset
+    PreparedStatement stmt = null;
+    public int userIDLOG = 0;
+    /**
+     * Connect to a sample database
+     */
+
+    public Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:db/pfm.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /**
+     * Member Login
+     */
+
+    public boolean memberLogin(String user, String pass) {
+        String sql = "select * from user where email = ? and password = ? and admin = 0";
+        try (Connection conn = this.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            // QUERY RESULTS
+            stmt.setString(1, user);
+            stmt.setString(2, pass);
+            rs = stmt.executeQuery();
+            // FUNCTION
+            if (rs.next()) {
+                userID = Integer.parseInt(rs.getString("user_id"));
+                String Email = rs.getString("email");
+                String Admin = rs.getString("admin");
+                System.out.printf("FOUND ADMIN UserID = %s Email = %s Admin = %s", userID, Email, Admin);
+                return true;
+            }
+            // CLOSE CONNECTION
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
+        }
+        System.out.println("Operation adminLogin done successfully");
+        return false;
+    }
+    /**
+     * Admin Login
+     */
+
+    public boolean adminLogin(String user, String pass) {
+        String sql = "select * from user where email = ? and password = ? and admin = 1";
+        try (Connection conn = this.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            // QUERY RESULTS
+            stmt.setString(1, user);
+            stmt.setString(2, pass);
+            rs = stmt.executeQuery();
+            // FUNCTION
+            if (rs.next()) {
+                userID = Integer.parseInt(rs.getString("user_id"));
+                String Email = rs.getString("email");
+                String Admin = rs.getString("admin");
+                System.out.printf("FOUND ADMIN UserID = %s Email = %s Admin = %s", userID, Email, Admin);
+                return true;
+            }
+            // CLOSE CONNECTION
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
+        }
+        System.out.println("Operation adminLogin done successfully");
+        return false;
+    }
+
 }
 
